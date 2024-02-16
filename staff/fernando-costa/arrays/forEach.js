73e492991f802fe7f62844f5198e41fdf4b34503
fercostacastro <fernandocosta3000@gmail.com>
2024-02-16 14:01:16 +0100

    add arrays code #204

---
+++ staff/fernando-costa/arrays/forEach.js
@@ -0,0 +1,17 @@
+delete Array.prototype.forEach
+
+function forEach(array, callback){
+    for (var i = 0; i < array.length; i++)
+    var element = array[i]
+
+    callback(element, i, array)
+}
+
+console.log('CASE 1')
+
+var nums = [10, 20, 30, 40, 50]
+
+forEach(nums, function (num){
+    console.log(num);
+})
+
