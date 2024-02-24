0668b0b00eb1764734d98be3ca698ff1fa31e7bf
fercostacastro <fernandocosta3000@gmail.com>
2024-02-24 18:23:54 +0100

    add arrays #204

--- staff/fernando-costa/arrays/indexof.js
+++ staff/fernando-costa/arrays/indexof.js
@@ -3,6 +3,7 @@ delete Array.prototype.indexOf
 function indexOf(array, searchElement) {
   debugger;
   for (var i = 0; i < array.length; i++) {
+
     if (array[i] === searchElement) {
       return i
     }
@@ -18,3 +19,10 @@ var index = indexOf(animals, 'rabbit')
 
 console.assert(index === 2)
 // 2
+
+console.assert(animals[0] ==='dog', 'dog')
+console.assert(animals[1] === 'cat', 'cat')
+console.assert(animals[2] === 'rabbit', 'rabbit')
+console.assert(animals[3] === 'mouse', 'mouse')
+console.assert(animals[4] === 'piggeon', 'piggeon')
+console.assert(animals.length === 5, 5)
--- staff/fernando-costa/arrays/lastIndexOf.js
+++ staff/fernando-costa/arrays/lastIndexOf.js
@@ -1,9 +1,9 @@
-delete Array.prototype.indexOf
+delete Array.prototype.lastIndexOf
 
 function lastIndexOf(array, searchElement){
     for (var i = array.length - 1; i >= 0; i--) {
 
-        if (array[i -1] === searchElement){
+        if (array[i] === searchElement){
             return i
         }
     }
@@ -17,4 +17,28 @@ console.log('CASE 1')
 var animals = ['dog', 'cat', 'rabbit', 'mouse', 'piggeon']
 var index = lastIndexOf(animals, 'mouse')
 
-console.log(index)
+console.assert(index)
+// 3
+
+console.assert(animals[0] ==='dog', 'dog')
+console.assert(animals[1] === 'cat', 'cat')
+console.assert(animals[2] === 'rabbit', 'rabbit')
+console.assert(animals[3] === 'mouse', 'mouse')
+console.assert(animals[4] === 'piggeon', 'piggeon')
+console.assert(animals.length === 5, 5)
+
+
+console.log('CASE 2')
+
+var nums = [20, 33, 101, 55, 89]
+var index = lastIndexOf(nums, 200)
+
+console.assert(index)
+// -1
+
+console.assert(nums[0] === 20, 20)
+console.assert(nums[1] === 33, 33)
+console.assert(nums[2] === 101, 101)
+console.assert(nums[3] === 55, 55)
+console.assert(nums[4] === 89, 89)
+console.assert(nums.length === 5, 5)
