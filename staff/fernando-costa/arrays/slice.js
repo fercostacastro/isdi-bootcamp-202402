32607576266675a78bd0ddd6e96ea6367f40aa09
fercostacastro <fernandocosta3000@gmail.com>
2024-02-27 20:50:58 +0100

    add arrays #204

--- staff/fernando-costa/arrays/slice.js
+++ staff/fernando-costa/arrays/slice.js
@@ -1,7 +1,28 @@
 delete Array.prototype.slice
 
-function slice(index, end, start){
+function slice(array, start, end) {
+    var length = array.length;
 
+    if (start < 0) {
+        start += length;
+        if (start < 0) {
+            start = 0
+        }
+    }
+
+    if (end < 0) {
+        end += length;
+        if (end < 0) {
+            end = 0
+        }
+    }
+
+    var slicedArray = [];
+    for (var i = start; i < end && i < length; i++) {
+        slicedArray.push(array[i])
+    }
+
+    return slicedArray
 }
 
 
@@ -10,5 +31,25 @@ console.log('CASE 1')
 var animals = ['ant', 'bison', 'camel', 'duck', 'elephant']
 var result = slice(animals, 1, 5)
 
-console.log(result);
-// ["bison", "camel", "duck", "elephant"]
+console.assert(result);
+// ['bison', 'camel', 'duck', 'elephant']
+console.assert(animals[0] === 'ant', 'ant')
+console.assert(animals[1] === 'bison', 'bison')
+console.assert(animals[2] === 'camel', 'camel')
+console.assert(animals[3] === 'duck', 'duck')
+console.assert(animals[4] === 'elephant', 'elephant')
+console.assert(animals.length === 5, 5)
+
+console.log('CASE 2')
+
+var colors = ['blue', 'orange', 'red', 'pink', 'green']
+var result = slice(colors, 2, 4)
+
+console.assert(result)
+// ['red', 'pink']
+console.assert(colors[0] === 'blue', 'blue')
+console.assert(colors[1] === 'orange', 'orange')
+console.assert(colors[2] === 'red', 'red')
+console.assert(colors[3] === 'pink', 'pink')
+console.assert(colors[4] === 'green', 'green')
+console.assert(colors.length === 5, 5)
