a12afb14d0386c714a69944a8898608b2f5ae64e
fercostacastro <fernandocosta3000@gmail.com>
2024-02-27 20:01:41 +0100

    add prototypes #384

--- staff/fernando-costa/prototypes/person/Person.js
+++ staff/fernando-costa/prototypes/person/Person.js
@@ -3,10 +3,31 @@ function Person(name, lastName, skinColor, height, year, place, profession) {
     this.lastName = lastName
     this.skinColor = skinColor
     this.height = height
+    this.weight = weight
     this.yearOfBirth = year
+    this.growUp = age
     this.placeOfBirth = place
-    this.profession = this.profession
 }
 
+Person.prototype.height = function (height) {
+    this.height = height
+
+    if (typeof height !== 'number') {
+        throw new TypeError (height + ' is not a number')
+    } else if (height < 170 || height > 170 ) {
+        throw new TypeError (height + ' is not the real one')
+    } else {
+        this.height = 170
+    }
+}
+
+Person.prototype.weight = function (weight) {
+    this.weight = weight
+
+}
+
+Person.prototype.year = function (year) {
+    this.yearOfBirth = year
+}
 
 module.exports = Person
--- staff/fernando-costa/prototypes/person/Person.test.js
+++ staff/fernando-costa/prototypes/person/Person.test.js
@@ -2,11 +2,22 @@ var Person = require('./Person')
 
 console.log('TEST Person')
 
-var person = new Person('Nelson', 'Mandela', 'black', 170, 1918, 'SouthAfrica', 'politician')
+var person = new Person('Lindovaldo', 'Soritulla', 'black', 170, 80, 2000, 'Spain')
 
-console.assert(person.firstName === 'Nelson', 'firstName is Nelson')
-console.assert(person.lastName === 'Mandela', 'lastName is Mandela')
+console.assert(person.firstName === 'Lindovaldo', 'firstName is Lindovaldo')
+console.assert(person.lastName === 'Soritulla', 'lastName is Soritulla')
 console.assert(person.skinColor === 'black', 'skincolor is black')
-console.assert(person.height === 'year', 'year of birth is 1918')
-console.assert(person.place === 'SouthAfrica', 'place of birth is SouthAfrica')
-console.assert(person.profession === 'politician', 'profession is politician')
+console.assert(person.height === 170, 'height is 170cm')
+console.assert(person.weight === 80, 'weight is 80kg')
+console.assert(person.yearOfBirth === 2000, 'year of birth is 2001')
+console.assert(person.place === 'Spain', 'place of birth is Spain')
+
+console.log('CASE method height')
+
+var person = new Person('Jonderlay', 'Forcino')
+
+person.year = 0
+
+person.age(24)
+
+console.assert(person.yearsGrowUp === 24, 'age is 24')
