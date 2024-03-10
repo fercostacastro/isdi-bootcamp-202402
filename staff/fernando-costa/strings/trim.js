d79cf776c7a44ed7ca8d33a653d46f96a2c2a12f
fercostacastro <fernandocosta3000@gmail.com>
2024-03-10 14:00:14 +0100

    update strings #184

--- staff/fernando-costa/strings/trim.js
+++ staff/fernando-costa/strings/trim.js
@@ -1,7 +1,15 @@
 delete String.prototype.trim
 
 function trim(string) {
-    
+    var trimString = ''
+
+    for (var i = 0; i < string.length; i++) {
+        if (string[i] === '') {
+            trimString += string
+        }
+    }
+
+    return string
 }
 
 console.log('CASE 1');
