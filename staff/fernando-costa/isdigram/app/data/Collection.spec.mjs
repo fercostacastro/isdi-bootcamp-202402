e4368a589b16b4aa548b7e8f680441e4670d5f18
fercostacastro <fernandocosta3000@gmail.com>
2024-03-15 18:44:12 +0100

    update isdigram #322

--- staff/fernando-costa/isdigram/app/data/Collection.spec.mjs
+++ staff/fernando-costa/isdigram/app/data/Collection.spec.mjs
@@ -1,22 +1,24 @@
-describe('Collection', function () {
-    describe('constructor', function () {
-        it('creates a collection', function () {
-            var cars = new Collection('cars')
+import Collection from './Collection.mjs'
+
+describe('Collection', () => {
+    describe('constructor', () => {
+        it('creates a collection', () => {
+            const cars = new Collection('cars')
 
             expect(cars).toBeInstanceOf(Collection)
         })
     })
 
-    describe('> helpers', function () {
-        describe('_generateId', function () {
-            it('generates a random id', function () {
-                var cars = new Collection('cars')
+    describe('> helpers', () => {
+        describe('_generateId', () => {
+            it('generates a random id', () => {
+                const cars = new Collection('cars')
 
-                var id1 = cars._generateId()
+                const id1 = cars._generateId()
 
                 expect(typeof id1).toBe('string')
 
-                var id2 = cars._generateId()
+                const id2 = cars._generateId()
 
                 expect(typeof id2).toBe('string')
 
@@ -24,63 +26,63 @@ describe('Collection', function () {
             })
         })
 
-        describe('_loadDocuments', function () {
-            it('loads empty array on new collection', function () {
+        describe('_loadDocuments', () => {
+            it('loads empty array on new collection', () =>  {
                 delete localStorage.cars
 
-                var cars = new Collection('cars')
+                const cars = new Collection('cars')
 
-                var documents = cars._loadDocuments()
+                const documents = cars._loadDocuments()
 
                 expect(documents).toBeInstanceOf(Array)
                 expect(documents.length).toBe(0)
             })
 
-            it('loads data on non-empty collection', function () {
+            it('loads data on non-empty collection', () => {
                 localStorage.cars = '[{"brand":"porsche","model":"911"},{"brand":"fiat","model":"500"}]'
 
-                var cars = new Collection('cars')
+                const cars = new Collection('cars')
 
-                var documents = cars._loadDocuments()
+                const documents = cars._loadDocuments()
 
                 expect(documents).toBeInstanceOf(Array)
                 expect(documents.length).toBe(2)
 
-                var document = documents[0]
+                let document = documents[0]
                 expect(document).toBeInstanceOf(Object)
                 expect(document.brand).toBe('porsche')
                 expect(document.model).toBe('911')
 
-                var document = documents[1]
+                document = documents[1]
                 expect(document.brand).toBe('fiat')
                 expect(document.model).toBe('500')
             })
         })
 
-        describe('_saveDocuments', function () {
-            it('saves a collection', function () {
+        describe('_saveDocuments', () => {
+            it('saves a collection', () => {
                 delete localStorage.cars
 
-                var documents = [{ brand: 'porsche', model: '911' }, { brand: 'fiat', model: '500' }]
+                const documents = [{ brand: 'porsche', model: '911' }, { brand: 'fiat', model: '500' }]
 
-                var cars = new Collection('cars')
+                const cars = new Collection('cars')
 
                 cars._saveDocuments(documents)
 
                 //expect(!!localStorage.cars).toBe(true)
                 expect(typeof localStorage.cars).toBe('string')
 
-                var documentsJSON = '[{"brand":"porsche","model":"911"},{"brand":"fiat","model":"500"}]'
+                const documentsJSON = '[{"brand":"porsche","model":"911"},{"brand":"fiat","model":"500"}]'
 
                 expect(localStorage.cars).toBe(documentsJSON)
             })
 
-            it('fails on non-array documents', function () {
-                var documents = 'hola documents'
+            it('fails on non-array documents', () => {
+                const documents = 'hola documents'
 
-                var cars = new Collection('cars')
+                const cars = new Collection('cars')
 
-                var errorThrown
+                let errorThrown
 
                 try {
                     cars._saveDocuments(documents)
@@ -92,12 +94,12 @@ describe('Collection', function () {
                 expect(errorThrown.message).toBe('documents is not an array')
             })
 
-            it('fails on array with non-object document in documents', function () {
-                var documents = [{ brand: 'porsche', model: '911' }, { brand: 'fiat', model: '500' }, 'hola document']
+            it('fails on array with non-object document in documents', () => {
+                const documents = [{ brand: 'porsche', model: '911' }, { brand: 'fiat', model: '500' }, 'hola document']
 
-                var cars = new Collection('cars')
+                const cars = new Collection('cars')
 
-                var errorThrown
+                let errorThrown
 
                 try {
                     cars._saveDocuments(documents)
@@ -110,13 +112,13 @@ describe('Collection', function () {
             })
         })
 
-        describe('_backup', function () {
-            it('backs up the collection json', function () {
+        describe('_backup', () => {
+            it('backs up the collection json', () => {
                 delete localStorage['cars-backup']
 
                 localStorage.cars = '[{"brand":"porsche","model":"911"},{"brand":"fiat","model":"500"}]'
 
-                var cars = new Collection('cars')
+                const cars = new Collection('cars')
 
                 cars._backup()
 
@@ -124,13 +126,13 @@ describe('Collection', function () {
             })
         })
 
-        describe('_restore', function () {
-            it('restores the collection json', function () {
+        describe('_restore', () => {
+            it('restores the collection json', () => {
                 delete localStorage['cars']
 
                 localStorage['cars-backup'] = '[{"brand":"porsche","model":"911"},{"brand":"fiat","model":"500"}]'
 
-                var cars = new Collection('cars')
+                const cars = new Collection('cars')
 
                 cars._restore()
 
@@ -139,24 +141,24 @@ describe('Collection', function () {
         })
     })
 
-    describe('> CRUD', function () {
-        describe('findOne', function () {
-            it('should find an existing document', function () {
+    describe('> CRUD', () => {
+        describe('findOne', () => {
+            it('should find an existing document', () => {
                 localStorage.cars = '[{"brand":"porsche","model":"911"},{"brand":"fiat","model":"500"}]'
 
-                var cars = new Collection('cars')
+                const cars = new Collection('cars')
 
-                var car = cars.findOne(function (car) { return car.brand === 'fiat' })
+                const car = cars.findOne(car => car.brand === 'fiat' )
 
                 expect(car).toBeInstanceOf(Object)
                 expect(car.brand).toBe('fiat')
                 expect(car.model).toBe('500')
             })
 
-            it('should fail on no callback', function () {
-                var cars = new Collection('cars')
+            it('should fail on no callback', () => {
+                const cars = new Collection('cars')
 
-                var errorThrown
+                let errorThrown
 
                 try {
                     cars.findOne()
@@ -168,10 +170,10 @@ describe('Collection', function () {
                 expect(errorThrown.message).toBe('callback is not a function')
             })
 
-            it('should fail on non-function callback', function () {
-                var cars = new Collection('cars')
+            it('should fail on non-function callback', () => {
+                const cars = new Collection('cars')
 
-                var errorThrown
+                let errorThrown
 
                 try {
                     cars.findOne(123)
@@ -184,53 +186,53 @@ describe('Collection', function () {
             })
         })
         // TODO test all methods
-        describe('insertOne', function () {
-            it('should insert a new document', function () {
+        describe('insertOne', () => {
+            it('should insert a new document', () => {
                 localStorage.cars = '[{"brand":"porsche","model":"911"},{"brand":"fiat","model":"500"}]'
 
-                var cars = new Collection('cars')
+                const cars = new Collection('cars')
 
-                var car = cars.insertOne({brand: "ford", model: "fiesta"})
+                const car = cars.insertOne({brand: "ford", model: "fiesta"})
 
-                var documents = cars._loadDocuments()
+                const documents = cars._loadDocuments()
 
                 expect(documents).toBeInstanceOf(Array)
                 expect(documents.length).toBe(3)
 
-                var document = documents[0]
+                let document = documents[0]
                 expect(document).toBeInstanceOf(Object)
                 expect(document.brand).toBe('porsche')
                 expect(document.model).toBe('911')
 
-                var document = documents[1]
+                document = documents[1]
                 expect(document.brand).toBe('fiat')
                 expect(document.model).toBe('500')
 
-                var document = documents[2]
+                document = documents[2]
                 expect(document.brand).toBe('ford')
                 expect(document.model).toBe('fiesta')
             })
         })
 
-        describe('updateOne', function () {
-            it('should update a new document', function () {
+        describe('updateOne', () => {
+            it('should update a new document', () => {
                 localStorage.cars = '[{"brand":"porsche","model":"911","id":"1"},{"brand":"fiat","model":"500","id":"2"}]'
 
-                var cars = new Collection('cars')
+                const cars = new Collection('cars')
 
-                var car = cars.updateOne({brand: "ferrari", model: "testarossa", id: "2"})
+                const car = cars.updateOne({brand: "ferrari", model: "testarossa", id: "2"})
 
-                var documents = cars._loadDocuments()
+                let documents = cars._loadDocuments()
 
                 expect(documents).toBeInstanceOf(Array)
                 expect(documents.length).toBe(2)
 
-                var document = documents[0]
+                document = documents[0]
                 expect(document).toBeInstanceOf(Object)
                 expect(document.brand).toBe('porsche')
                 expect(document.model).toBe('911')
 
-                var document = documents[1]
+                document = documents[1]
                 expect(document.brand).toBe('ferrari')
                 expect(document.model).toBe('testarossa')
             })
--- staff/fernando-costa/isdigram/app/logic.spec.mjs
+++ staff/fernando-costa/isdigram/app/logic.spec.mjs
@@ -1,15 +1,14 @@
-describe('logic', function () {
-    describe('registerUser', function () {
-        it('succeeds a new user', function () {
-            db.users.deleteOne(function (user) {
-                return user.username === 'peperoni'
-            })
+import db from './data/index.mjs'
+import logic from './logic.mjs'
+
+describe('logic', () => {
+    describe('registerUser', () => {
+        it('succeeds a new user', () => {
+            db.users.deleteOne(user => user.username === 'peperoni')
 
             logic.registerUser('Pepe Roni', '2000-01-01', 'pepe@roni.com', 'peperoni', '123qwe123')
 
-            var user = db.users.findOne(function (user) {
-                return user.username === 'peperoni'
-            })
+            const user = db.users.findOne(user => user.username === 'peperoni')
 
             expect(!!user).toBe(true)
             expect(user.name).toBe('Pepe Roni')
@@ -19,14 +18,12 @@ describe('logic', function () {
             expect(user.password).toBe('123qwe123')
         })
 
-        it('fails on existing users', function () {
-            db.users.deleteOne(function (user) {
-                return user.username === 'peperoni'
-            })
+        it('fails on existing users', () => {
+            db.users.deleteOne(user => user.username === 'peperoni')
 
             db.users.insertOne({ name: 'Pepe Roni', birthdate: '2000-01-01', email: 'pepe@roni.com', username: 'peperoni', password: '123qwe123' })
 
-            var errorThrown
+            let errorThrown
 
             try {
                 logic.registerUser('Pepe Roni', '2000-01-01', 'pepe@roni.com', 'peperoni', '123qwe123')
@@ -38,8 +35,8 @@ describe('logic', function () {
             expect(errorThrown.message).toBe('user already exists')
         })
 
-        it('fails on non string name', function () {
-            var errorThrown
+        it('fails on non string name', () => {
+            let errorThrown
 
             try {
                 logic.registerUser(123, '2000-01-01', 'pepe@roni.com', 'peperoni', '123qwe123')
@@ -51,8 +48,8 @@ describe('logic', function () {
             expect(errorThrown.message).toBe('name 123 is not a string')
         })
 
-        it('fails on empty name', function () {
-            var errorThrown
+        it('fails on empty name', () => {
+            let errorThrown
 
             try {
                 logic.registerUser('', '2000-01-01', 'pepe@roni.com', 'peperoni', '123qwe123')
@@ -64,8 +61,8 @@ describe('logic', function () {
             expect(errorThrown.message).toBe('name >< is empty or blank')
         })
 
-        it('fails on non string birthdate', function () {
-            var errorThrown
+        it('fails on non string birthdate', () => {
+            let errorThrown
 
             try {
                 logic.registerUser('Pepe Roni', 123, 'pepe@roni.com', 'peperoni', '123qwe123')
@@ -77,8 +74,8 @@ describe('logic', function () {
             expect(errorThrown.message).toBe('birthdate 123 is not a string')
         })
 
-        it('fails on incorrect birthdate format', function () {
-            var errorThrown
+        it('fails on incorrect birthdate format', () => {
+            let errorThrown
 
             try {
                 logic.registerUser('Pepe Roni', '2000/01/01', 'pepe@roni.com', 'peperoni', '123qwe123')
@@ -93,35 +90,29 @@ describe('logic', function () {
         // TODO add other unhappy test cases
     })
 
-    describe('loginUser', function () {
-        it('succeeds on existing user and correct credentials', function () {
-            db.users.deleteOne(function (user) {
-                return user.username === 'peperoni'
-            })
+    describe('loginUser', () => {
+        it('succeeds on existing user and correct credentials', () => {
+            db.users.deleteOne(user => user.username === 'peperoni')
 
             db.users.insertOne({ name: 'Pepe Roni', birthdate: '2000-01-01', email: 'pepe@roni.com', username: 'peperoni', password: '123qwe123' })
 
-            var user = db.users.findOne(function (user) { return user.username === 'peperoni' })
+            let user = db.users.findOne(user => user.username === 'peperoni')
 
             logic.loginUser('peperoni', '123qwe123')
 
             expect(sessionStorage.userId).toBe(user.id)
 
-            var user = db.users.findOne(function (user) {
-                return user.id === sessionStorage.userId
-            })
+            user = db.users.findOne(user => user.id === sessionStorage.userId)
 
             expect(user.status).toBe('online')
         })
 
-        it('fails on existing user and incorrect password', function () {
-            db.users.deleteOne(function (user) {
-                return user.username === 'peperoni'
-            })
+        it('fails on existing user and incorrect password', () => {
+            db.users.deleteOne(user => user.username === 'peperoni')
 
             db.users.insertOne({ name: 'Pepe Roni', birthdate: '2000-01-01', email: 'pepe@roni.com', username: 'peperoni', password: '123qwe123' })
 
-            var errorThrown
+            let errorThrown
 
             try {
                 logic.loginUser('peperoni', '123qwe123qwe')
@@ -133,14 +124,12 @@ describe('logic', function () {
             expect(errorThrown.message).toBe('wrong password')
         })
 
-        it('fails on existing user and incorrect username', function () {
-            db.users.deleteOne(function (user) {
-                return user.username === 'peperoni'
-            })
+        it('fails on existing user and incorrect username', () => {
+            db.users.deleteOne(user => user.username === 'peperoni')
 
             db.users.insertOne({ name: 'Pepe Roni', birthdate: '2000-01-01', email: 'pepe@roni.com', username: 'peperoni', password: '123qwe123' })
 
-            var errorThrown
+            let errorThrown
 
             try {
                 logic.loginUser('peperoni2', '123qwe123')
@@ -150,14 +139,6 @@ describe('logic', function () {
 
             expect(errorThrown).toBeInstanceOf(Error)
             expect(errorThrown.message).toBe('user not found')
-        })
-    })
-
-    describe('retrieveUser', function () {
-        it('should return a user, if it exists', function () {
-            db.users.findOne(function (user) {
-                return user.username = 'xavi'
-            })
         })
     })
 
